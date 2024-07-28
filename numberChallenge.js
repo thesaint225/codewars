190fb7f0bc8b9ac4a984480216970f5ef7bb60d6
henrymiessan <miessanhenri@gmail.com>
2024-07-28 01:00:40 +0000

    Implement function to return middle character(s) of a word based on length

--- numberChallenge.js
+++ numberChallenge.js
@@ -1,6 +1,20 @@
-var x = Math.floor(Math.random() * 100 + 1);
-console.log(x);
-var y = Math.floor(Math.random() * 50 + 1);
-console.log(y);
-var sumOutput = x + y;
-console.log(sumOutput);
+// const x: number = Math.floor(Math.random() * 100 + 1);
+// console.log(x);
+// const y: number = Math.floor(Math.random() * 50 + 1);
+// console.log(y);
+// const sumOutput: number = x + y;
+// console.log(sumOutput);
+var x = "testing";
+var arr = x.split("");
+var y = Math.floor(arr.length / 2);
+if (arr.length % 2 !== 0) {
+    // If the length of the array is odd, log the middle character
+    console.log(arr[y]);
+}
+else {
+    // If the length is even, indicate it is not applicable
+    console.log("Not applicable for even-length words.");
+}
+// console.log(y);
+// const y = "cat"[1];
+// console.log(y);
--- numberChallenge.ts
+++ numberChallenge.ts
@@ -1,8 +1,37 @@
-const x: number = Math.floor(Math.random() * 100 + 1);
-console.log(x);
+// const x: number = Math.floor(Math.random() * 100 + 1);
+// console.log(x);
 
-const y: number = Math.floor(Math.random() * 50 + 1);
-console.log(y);
+// const y: number = Math.floor(Math.random() * 50 + 1);
+// console.log(y);
 
-const sumOutput: number = x + y;
-console.log(sumOutput);
+// const sumOutput: number = x + y;
+// console.log(sumOutput);
+
+// let x: string = "testing";
+// let arr: string[] = x.split("");
+// let y: number = Math.floor(arr.length / 2);
+// if (arr.length % 2 !== 0) {
+//   // If the length of the array is odd, log the middle character
+//   console.log(arr[y]);
+// } else {
+//   // If the length is even, indicate it is not applicable
+//   console.log("Not applicable for even-length words.");
+// }
+
+function getMiddleCharacter(word: string): string {
+  const length = word.length;
+  const middleIndex = Math.floor(length / 2);
+
+  if (length % 2 === 0) {
+    // Even length: return the middle two characters
+    return word[middleIndex - 1] + word[middleIndex];
+  } else {
+    // Odd length: return the middle character
+    return word[middleIndex];
+  }
+}
+
+// console.log(y);
+
+// const y = "cat"[1];
+// console.log(y);
